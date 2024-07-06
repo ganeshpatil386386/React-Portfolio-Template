16059596504359bb9c425f75483bf24e479506fa
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2024-07-06 19:30:33 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
 import Blogs from "./pages/Blogs";
@@ -15,23 +16,25 @@ function App() {
 
   return (
     <Router>
-      <div className={isDark ? "dark" : ""}>
-        <div
-          className={`min-h-screen flex flex-col ${
-            isDark
-              ? "bg-slate-800 text-white"
-              : "bg-slate-100 text-black overflow-auto"
-          }`}
-        >
-          <Navbar isDark={isDark} toggleDark={toggleDark} />
-          <Routes>
-            <Route path='/blogs' element={<Blogs />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/youtube' element={<Youtube />} />
-            <Route path='/' element={<Content />} />
-          </Routes>
+      <Analytics>
+        <div className={isDark ? "dark" : ""}>
+          <div
+            className={`min-h-screen flex flex-col ${
+              isDark
+                ? "bg-slate-800 text-white"
+                : "bg-slate-100 text-black overflow-auto"
+            }`}
+          >
+            <Navbar isDark={isDark} toggleDark={toggleDark} />
+            <Routes>
+              <Route path='/blogs' element={<Blogs />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/youtube' element={<Youtube />} />
+              <Route path='/' element={<Content />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Analytics>
     </Router>
   );
 }
